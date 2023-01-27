539080d73318bf1aa1ce45666e5a82301b84b650
smzahraee <zahraee.sm@gmail.com>
2023-01-27 17:58:49 +0330

    decrease build time of go-fula in downloading modules enable go-fula version selection

--- docker/go-fula/build.sh
+++ docker/go-fula/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#before runnig following commands run bellow command for enabling push to dockerhub
-#docker login --username=smzahraee
+git clone -b $GO_FULA_BRANCH https://github.com/functionland/go-fula
 
-git clone https://github.com/functionland/go-fula
-
-docker buildx create --name multiarch --driver docker-container --use
-
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t smzahraee/go-fula:latest --push .
+docker buildx build --platform $ARCH_SUPPORT -t $GO_FULA_IMAGE:latest --push .
