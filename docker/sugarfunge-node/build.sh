eb39df73e4cbfe523c523b5e98287905d6f583df
smzahraee <zahraee.sm@gmail.com>
2023-02-18 22:16:35 +0330

    BUGFIX wrong copy path of node api final exe faster build for node and node-api

--- docker/sugarfunge-node/build.sh
+++ docker/sugarfunge-node/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git clone -b $SUGARFUNGE_NODE_BRANCH https://github.com/functionland/sugarfunge-node
+git clone -b $SUGARFUNGE_API_BRANCH https://github.com/functionland/sugarfunge-api
+
 docker buildx build --platform $ARCH_SUPPORT -t $SUGARFUNGE_NODE_IMAGE:$GSUGARFUNGE_NODE_DOCKER_TAG --push .
