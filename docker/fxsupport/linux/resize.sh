9f44724735cecd780f7a87c064ca8320028456ee
ehsan shariati <ehsan6sha@gmail.com>
2023-04-10 02:03:51 +0430

    Update resize.sh

--- docker/fxsupport/linux/resize.sh
+++ docker/fxsupport/linux/resize.sh
@@ -4,7 +4,10 @@ resize_flag=/usr/bin/fula/.resize_flg
 
 resize_rootfs () {
   touch /usr/bin/fula/.resize_flg
-  sh /usr/lib/raspi-config/init_resize.sh
+  #sh /usr/lib/raspi-config/init_resize.sh
+  sudo raspi-config --expand-rootfs
+  echo "Rootfs expanded..."
+  sudo reboot
   exit 0
 }
 
