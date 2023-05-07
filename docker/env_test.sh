9765d734ebbbd2a4a3407bcb9a12fc4505d48d06
ehsan shariati <ehsan6sha@gmail.com>
2023-05-07 15:11:30 -0400

    replaced CURRENT_USER with pi

--- docker/env_test.sh
+++ docker/env_test.sh
@@ -2,8 +2,8 @@
 export ARCH_SUPPORT="linux/arm64"
 
 export DOCKER_REPO="functionland"
-export DEFAULT_FX_TAG="test30"
-export DEFAULT_FULA_TAG="test30"
+export DEFAULT_FX_TAG="test31"
+export DEFAULT_FULA_TAG="test31"
 export DEFAULT_NODE_TAG="release"
 
 #build fxsupport
