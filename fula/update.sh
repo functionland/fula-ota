d4805e1468567bf4da070ac6b11c49bda6685b30
ehsan shariati <ehsan6sha@gmail.com>
2023-06-25 17:03:26 +0330

    sync fula with linux folder

--- fula/update.sh
+++ fula/update.sh
@@ -14,17 +14,18 @@ for device in "${devices[@]}"; do
 
         # Check if the update file exists on this device
         if [ -f "$mountpoint/fula_update/update.yaml" ]; then
+            mv "$mountpoint/fula_update/update.yaml" "$mountpoint/fula_update/update.inprogress.yaml"
             sudo rm /home/pi/stop_docker_copy.txt
 
             if [ -f "$mountpoint/fula_update/repair_init.sh" ]; then
-                sudo bash "$mountpoint/fula_update/repair_init.sh" 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Error Running repair_init" >> $FULA_LOG_PATH 2>&1; exit 1; }
+                sudo bash "$mountpoint/fula_update/repair_init.sh" 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Error Running repair_init" >> $FULA_LOG_PATH 2>&1; }
             fi
 
             if pgrep -f "control_led.py" > /dev/null; then
-                sudo pkill -f "control_led.py" || { echo "Error Killing control_led" >> $FULA_LOG_PATH 2>&1; exit 1; }
+                sudo pkill -f "control_led.py" || { echo "Error Killing control_led" >> $FULA_LOG_PATH 2>&1; }
             fi
 
-            python /usr/bin/fula/control_led.py blue 100 >> $FULA_LOG_PATH 2>&1 &
+            python /usr/bin/fula/control_led.py blue 200 >> $FULA_LOG_PATH 2>&1 &
             sudo systemctl stop fula
 
             if [ -d "$mountpoint/fula_update/fula" ]; then
@@ -33,7 +34,8 @@ for device in "${devices[@]}"; do
             else
                 echo "Error: fula_update/fula directory not found on device $device" >> $FULA_LOG_PATH 2>&1
                 sudo cp /home/pi/fula.sh.log* "$mountpoint/fula_update/"
-                mv "$mountpoint/fula_update/update.yaml" "$mountpoint/fula_update/update.error.yaml"
+                mv "$mountpoint/fula_update/update.inprogress.yaml" "$mountpoint/fula_update/update.error.yaml"
+                python /usr/bin/fula/control_led.py red 3 >> $FULA_LOG_PATH 2>&1
                 exit 1;
             fi
 
@@ -50,18 +52,20 @@ for device in "${devices[@]}"; do
                 fi
                 sudo cp /home/pi/fula.sh.log* "$mountpoint/fula_update/"
                 date | sudo tee -a /home/pi/stop_docker_copy.txt > /dev/null
-                mv "$mountpoint/fula_update/update.yaml" "$mountpoint/fula_update/update.completed.yaml"
+                mv "$mountpoint/fula_update/update.inprogress.yaml" "$mountpoint/fula_update/update.completed.yaml"
                 python /usr/bin/fula/control_led.py blue -1 >> $FULA_LOG_PATH 2>&1 &
 
                 if pgrep -f "control_led.py" > /dev/null; then
                     sudo pkill -f "control_led.py"
                 fi
                 sleep 2
+                python /usr/bin/fula/control_led.py green 3 >> $FULA_LOG_PATH 2>&1
                 sudo reboot
             else
                 echo "Error: unable to navigate to /usr/bin/fula" >> $FULA_LOG_PATH 2>&1
                 sudo cp /home/pi/fula.sh.log* "$mountpoint/fula_update/"
-                mv "$mountpoint/fula_update/update.yaml" "$mountpoint/fula_update/update.error.yaml"
+                mv "$mountpoint/fula_update/update.inprogress.yaml" "$mountpoint/fula_update/update.error.yaml"
+                python /usr/bin/fula/control_led.py red 3 >> $FULA_LOG_PATH 2>&1
                 exit 1;
             fi
         else
