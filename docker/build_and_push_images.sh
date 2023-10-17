3633492547667f9fb5ac3c703d1794177385fc62
ehsan shariati <ehsan6sha@gmail.com>
2023-10-17 09:47:29 -0400

    Update build_and_push_images.sh

--- docker/build_and_push_images.sh
+++ docker/build_and_push_images.sh
@@ -9,12 +9,12 @@ docker buildx create --name multiarch --driver docker-container --use
 
 #build fxsupport
 echo "Building $FX_SUPPORT_IMAGE ..."
-cd $SCRIPTS_DIR/fxsupport/ && ./build.sh
+cd $SCRIPTS_DIR/fxsupport/ && bash ./build.sh
 
 #build go-fula
 echo "Building $GO_FULA_IMAGE ..."
-cd $SCRIPTS_DIR/go-fula/ && ./build.sh
+cd $SCRIPTS_DIR/go-fula/ && bash ./build.sh
 
 #build node and node-api
 echo "Building $SUGARFUNGE_NODE_IMAGE ..."
-cd $SCRIPTS_DIR/sugarfunge-node/ && ./build.sh
+cd $SCRIPTS_DIR/sugarfunge-node/ && bash ./build.sh
