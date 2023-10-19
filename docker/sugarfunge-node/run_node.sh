bdbaa133ceb95190a33fb018d8114fa73c92a0d4
ehsan shariati <ehsan6sha@gmail.com>
2023-10-19 11:29:09 -0400

    node error correction
    1- added needed ports to fula_go
    2- added needed packages to fula_node
    3- added check to run api after node

--- docker/sugarfunge-node/run_node.sh
+++ docker/sugarfunge-node/run_node.sh
@@ -5,6 +5,17 @@ export NODE_PORT=9945
 /sugarfunge-node --chain /customSpecRaw.json --enable-offchain-indexing true --base-path=/uniondrive/chain --keystore-path=/internal/keys --port=30335 --rpc-port $NODE_PORT --rpc-external --rpc-cors=all --rpc-methods=Unsafe --name FulaNode --password-filename="/internal/.secrets/password.txt" --bootnodes /dns4/node.functionyard.fula.network/tcp/30334/p2p/12D3KooWBeXV65svCyknCvG1yLxXVFwRxzBLqvBJnUF6W84BLugv &
 NODE_PID=$!
 
+# Wait until the node is up and running (checks every second for up to 60 seconds)
+counter=0
+while ! nc -z 127.0.0.1 $NODE_PORT; do
+  sleep 1
+  counter=$((counter + 1))
+  if [ $counter -ge 60 ]; then
+    echo "Node service didn't start within 60 seconds. Exiting."
+    exit 1
+  fi
+done
+
 # Start the node api process
 /sugarfunge-api --db-uri=/data --node-server ws://127.0.0.1:$NODE_PORT &
 API_PID=$!
