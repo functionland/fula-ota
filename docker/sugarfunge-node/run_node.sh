54cab47016393b5a8e7e837dd197770b90f59cc0
ehsan shariati <ehsan6sha@gmail.com>
2023-10-29 20:34:09 -0400

    added needed steps for keys before running node

--- docker/sugarfunge-node/run_node.sh
+++ docker/sugarfunge-node/run_node.sh
@@ -4,14 +4,52 @@ export NODE_PORT=9945
 export IPFS_PORT=5001
 
 # Wait indefinitely until the password file and /uniondrive folder are available
-while [ ! -f "/internal/.secrets/password.txt" ] || [ ! -d "/uniondrive" ]; do
+while [ ! -f "/internal/box_props.json" ] || [ ! -d "/uniondrive" ]; do
   sleep 3
-  [ ! -f "/internal/.secrets/password.txt" ] && echo "Waiting for /internal/.secrets/password.txt to become available..."
+  [ ! -f "/internal/box_props.json" ] && echo "Waiting for /internal/box_props.json to become available..."
   [ ! -d "/uniondrive" ] && echo "Waiting for /uniondrive to become available..."
 done
 
+# Read blox_seed from JSON file
+blox_seed=$(jq -r '.blox_seed' /internal/box_props.json)
+
+# Create /internal/.secrets directory if it doesn't exist
+mkdir -p /internal/.secrets
+
+# Save blox_seed into password.txt
+echo "$blox_seed" > /internal/.secrets/password.txt
+
+#save the node key
+# Generate the key and save the output to a variable
+output=$(/sugarfunge-node key generate-node-key)
+
+# Extract the 'key' field from the output
+node_key=$(echo "$output" | grep "key:" | awk '{print $2}')
+
+#echo can be removed
+echo "$node_key"
+
+# create Aura and Grandpa keys
+# Generate the key and save the output to a variable
+output=$(/sugarfunge-node key generate --scheme Sr25519 --password-filename="/internal/.secrets/password.txt")
+
+# Extract the 'Secret phrase' field from the output
+secret_phrase=$(echo "$output" | grep "Secret phrase:" | awk '{$1=$2=""; print $0}' | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+# echo can be removed
+echo "$secret_phrase"  # You can save this to a file or use it as needed
+
+# create grandpa account
+/sugarfunge-node key inspect --password-filename="/internal/.secrets/password.txt" --scheme Ed25519 "$secret_phrase"
+
+#Add Aura key to keystore
+/sugarfunge-node key insert --base-path=/uniondrive/chain --keystore-path=/internal/keys --chain /customSpecRaw.json --scheme Sr25519 --suri "$secret_phrase" --password-filename="/internal/.secrets/password.txt" --key-type aura
+
+#Add Grandpa key to keystore
+./sugarfunge-node key insert --base-path=/uniondrive/chain --keystore-path=/internal/keys --chain /customSpecRaw.json --scheme Ed25519 --suri "$secret_phrase" --password-filename="/internal/.secrets/password.txt" --key-type gran
+
 # Start the node process
-/sugarfunge-node --chain /customSpecRaw.json --enable-offchain-indexing true --base-path=/uniondrive/chain --keystore-path=/internal/keys --port=30335 --rpc-port $NODE_PORT --rpc-external --rpc-cors=all --rpc-methods=Unsafe --name FulaNode --password-filename="/internal/.secrets/password.txt" --bootnodes /dns4/node.functionyard.fula.network/tcp/30334/p2p/12D3KooWBeXV65svCyknCvG1yLxXVFwRxzBLqvBJnUF6W84BLugv &
+/sugarfunge-node --chain /customSpecRaw.json --enable-offchain-indexing true --base-path=/uniondrive/chain --keystore-path=/internal/keys --port=30335 --rpc-port $NODE_PORT --rpc-external --rpc-cors=all --rpc-methods=Unsafe --name FulaNode --password-filename="/internal/.secrets/password.txt" --bootnodes /dns4/node.functionyard.fula.network/tcp/30334/p2p/12D3KooWBeXV65svCyknCvG1yLxXVFwRxzBLqvBJnUF6W84BLugv --node-key=$node_key --offchain-worker always &
 NODE_PID=$!
 
 # Wait until the node is up and running (checks every second for up to 120 seconds)
@@ -35,8 +73,7 @@ while ! nc -z 127.0.0.1 $IPFS_PORT; do
   echo "Waiting for port 5001 to be available..."
 done
 
-Operator_seed=$(cat /internal/.secrets/seed.txt)
-/proof-engine -- $Operator_seed &
+/proof-engine -- $node_key &
 PROOF_ENGINE_PID=$!
 
 # Wait for any process to exit
