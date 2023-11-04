1d5ba4c6b01c423e8065532241e6906a03446980
ehsan shariati <ehsan6sha@gmail.com>
2023-11-04 14:30:22 -0400

    Update env_test.sh

--- docker/env_test.sh
+++ docker/env_test.sh
@@ -2,9 +2,9 @@
 export ARCH_SUPPORT="linux/arm64"
 
 export DOCKER_REPO="functionland"
-export DEFAULT_FX_TAG="test116"
-export DEFAULT_FULA_TAG="test116"
-export DEFAULT_NODE_TAG="test114"
+export DEFAULT_FX_TAG="test117"
+export DEFAULT_FULA_TAG="test117"
+export DEFAULT_NODE_TAG="test117"
 
 #build fxsupport
 export FX_SUPPORT_IMAGE="$DOCKER_REPO/fxsupport"
