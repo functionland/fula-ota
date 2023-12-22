386962b7e7ba1430e3d0ec70aa269a65d581c094
ehsan shariati <ehsan6sha@gmail.com>
2023-12-22 11:23:11 -0500

    added interface chekc to go-fula before wap

--- docker/go-fula/go-fula.sh
+++ docker/go-fula/go-fula.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
 check_wifi_name() {
     # Get the name of the currently active Wi-Fi connection.
     wifi_name=$(nmcli -g GENERAL.CONNECTION device show $1)
@@ -33,35 +37,80 @@ check_writable() {
   # Check if /internal exists and is writable
   if [ -d "/internal" ]; then
     if ! touch /internal/.tmp_write || ! rm /internal/.tmp_write; then
-      echo "/internal is not writable."
+      log "/internal is not writable."
       return 1
     fi
   else
-    echo "/internal does not exist."
+    log "/internal does not exist."
     return 1
   fi
 
   # Check if /uniondrive exists and is writable
   if [ -d "/uniondrive" ]; then
     if ! touch /uniondrive/.tmp_write || ! rm /uniondrive/.tmp_write; then
-      echo "/uniondrive is not writable."
+      log "/uniondrive is not writable."
       return 1
     fi
   else
-    echo "/uniondrive does not exist."
+    log "/uniondrive does not exist."
     return 1
   fi
 
-  echo "Both /internal and /uniondrive exist and are writable."
+  log "Both /internal and /uniondrive exist and are writable."
+  return 0
+}
+
+check_interfaces() {
+  # Check for required commands
+  if ! command -v ip &> /dev/null; then
+      log "The 'ip' command is required but not found. Exiting."
+      return 1
+  fi
+
+  if ! command -v iwconfig &> /dev/null; then
+      log "The 'iwconfig' command is required but not found. Exiting."
+      return 1
+  fi
+  # Get a list of all network interfaces
+  interfaces=$(iwconfig 2>&1 | grep 'IEEE' | awk '{print $1}')
+  if [ -z "$interfaces" ]; then
+      log "No wireless network interfaces found. Exiting."
+      return 1
+  fi
+
+  # Loop through each interface and check its status
+  for interface in $interfaces; do
+      log "Checking wireless interface: $interface"
+      while : ; do
+          # Get the current state of the interface
+          state=$(ip link show "$interface" | awk '/state UP/ {print $9}')
+
+          # Check if the interface is in the 'UP' state
+          if [ "$state" = "UP" ]; then
+              log "Interface $interface is up and ready."
+              break
+          else
+              log "Waiting for interface $interface to be ready..."
+              sleep 1
+          fi
+      done
+  done
+
+  # Wait an additional 5 seconds after all interfaces are up
+  log "All interfaces are ready."
   return 0
 }
 
 
 # Loop until /internal and /uniondrive are verified to exist and be writable
 while ! check_writable; do
-  echo "Waiting for /internal and /uniondrive to become writable..."
+  log "Waiting for /internal and /uniondrive to become writable..."
   sleep 5
 done
+
+check_interfaces
+log "Waiting an additional 5 seconds after all wifi interfaces are up"
+sleep 5
 
 wap_pid=0
 
@@ -70,24 +119,27 @@ wap_pid=$!
 
 while true; do
   if check_internet && check_files_exist; then
-    echo "Internet connected and necessary files exist. Running /app."
+    log "Internet connected and necessary files exist. Running /app."
     node_key_file="/internal/.secrets/node_key.txt"
     mkdir -p /internal/.secrets
     # Generate the node key
     new_key=$(/app --generateNodeKey | grep -E '^[a-f0-9]{64}$')
     # Check if the node_key file exists and has different content
     if [ ! -f "$node_key_file" ] || [ "$new_key" != "$(cat $node_key_file)" ]; then
-      echo "$new_key" > "$node_key_file"
-      echo "Node key saved to $node_key_file"
+      log "$new_key" > "$node_key_file"
+      log "Node key saved to $node_key_file"
     else
-      echo "Node key file already exists and is up to date."
+      log "Node key file already exists and is up to date."
     fi
     nmcli con down FxBlox
     /app --config /internal/config.yaml
     break
   elif [ $wap_pid -eq 0 ]; then
-    echo "Either Internet not connected or necessary files missing. Running /wap."
+    log "Either Internet not connected or necessary files missing. But wap is also not running. Reboot?"
+  else 
+    log "Either Internet not connected or necessary files missing"
   fi
 
   sleep 7
 done
+return 0
