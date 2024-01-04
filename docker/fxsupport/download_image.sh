d443851784922470fbb4408d852459266c7abe65
ehsan shariati <ehsan6sha@gmail.com>
2024-01-04 16:49:55 -0500

    download arm64 image from dokcer

--- docker/fxsupport/download_image.sh
+++ docker/fxsupport/download_image.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Pull the fxsupport image from Docker Hub
-docker pull $FX_SUPPORT_IMAGE:$FX_SUPPORT_DOCKER_TAG
+# Specify the desired platform
+PLATFORM="linux/arm64"
+
+# Pull the fxsupport image from Docker Hub for the specified platform
+docker pull --platform=$PLATFORM $FX_SUPPORT_IMAGE:$FX_SUPPORT_DOCKER_TAG
 
 # Save the image as a tar file
 mkdir -p dockers
--- docker/sugarfunge-node/download_image.sh
+++ docker/sugarfunge-node/download_image.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-# Pull the node image from Docker Hub
-docker pull $SUGARFUNGE_NODE_IMAGE:$SUGARFUNGE_NODE_DOCKER_TAG
+# Define the desired platform
+PLATFORM="linux/arm64"
+
+# Check if the necessary variables are set
+if [ -z "$SUGARFUNGE_NODE_IMAGE" ] || [ -z "$SUGARFUNGE_NODE_DOCKER_TAG" ]; then
+    echo "The variables SUGARFUNGE_NODE_IMAGE and SUGARFUNGE_NODE_DOCKER_TAG must be set."
+    exit 1
+fi
+
+# Pull the node image from Docker Hub for the specified platform
+docker pull --platform=$PLATFORM $SUGARFUNGE_NODE_IMAGE:$SUGARFUNGE_NODE_DOCKER_TAG
 
 # Save the image as a tar file
 mkdir -p dockers
 docker save $SUGARFUNGE_NODE_IMAGE:$SUGARFUNGE_NODE_DOCKER_TAG -o dockers/node.tar
+
+echo "Docker image saved to dockers/node.tar"
