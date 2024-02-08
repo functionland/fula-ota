cb2f3fea26f88308408fd7a16d159d0f4488a008
ehsan shariati <ehsan6sha@gmail.com>
2024-02-08 18:13:04 -0500

    added nvme to check-mount.sh

--- docker/fxsupport/linux/check-mount.sh
+++ docker/fxsupport/linux/check-mount.sh
@@ -14,10 +14,10 @@ mount_device() {
 
 # List all unmounted sd[a-z][0-9] devices and mount each one
 while IFS= read -r line; do
-    # Extract the device name
     DEVICE=$(awk '{print $1}' <<< "$line")
 
     mount_device "$DEVICE" &
-done < <(lsblk -nr -o NAME,MOUNTPOINT | awk '/^sd[a-z][0-9] / && $2 == ""')
+done < <(lsblk -nr -o NAME,MOUNTPOINT | awk '/^(sd[a-z][0-9]|nvme[0-9]n[0-9]p[0-9]) && $2 == ""')
+
 
 wait # Wait for all background processes to finish
