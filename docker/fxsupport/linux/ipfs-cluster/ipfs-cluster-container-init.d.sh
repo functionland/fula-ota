bdea6bd68091ebbc84070cf5e5b246b9d47511b7
ehsan shariati <ehsan6sha@gmail.com>
2024-03-04 09:40:49 -0500

    changed pool host url to pool-id.pools.

--- docker/fxsupport/linux/ipfs-cluster/ipfs-cluster-container-init.d.sh
+++ docker/fxsupport/linux/ipfs-cluster/ipfs-cluster-container-init.d.sh
@@ -111,7 +111,7 @@ if echo "$response" | grep -q 'HTTP/.* 200 OK'; then
         export CLUSTER_FOLLOWERMODE=true
         append_or_replace "/.env.cluster" "CLUSTER_FOLLOWERMODE" "${CLUSTER_FOLLOWERMODE}"
 
-        exec ipfs-cluster-service daemon --upgrade --bootstrap "/dns4/${CLUSTER_CRDT_TRUSTEDPEERS}.functionyard.fula.network/tcp/9096/ipfs/${CLUSTER_CRDT_TRUSTEDPEERS}" --leave
+        exec ipfs-cluster-service daemon --upgrade --bootstrap "/dns4/${poolName}.pools.functionyard.fula.network/tcp/9096/p2p/${CLUSTER_CRDT_TRUSTEDPEERS}" --leave
     else
         exec ipfs-cluster-service daemon --upgrade
     fi
