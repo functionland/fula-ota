cb8cba070fbd2445d5e7ae455be8e225e1ba9d81
ehsan shariati <ehsan6sha@gmail.com>
2024-07-14 18:54:11 -0400

    Update build.sh

--- docker/sugarfunge-node/build.sh
+++ docker/sugarfunge-node/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Clone and update the repositories
 git clone -b "$SUGARFUNGE_NODE_BRANCH" https://github.com/functionland/sugarfunge-node
 cd sugarfunge-node && git pull
 cd ..
@@ -10,4 +11,15 @@ git clone -b "$PROOF_ENGINE_BRANCH" https://github.com/functionland/proof-engine
 cd proof-engine && git pull
 cd ..
 
-docker buildx build --platform "$ARCH_SUPPORT" -t "$SUGARFUNGE_NODE_IMAGE":"$SUGARFUNGE_NODE_DOCKER_TAG" --push .
+# Extract TARGETARCH from ARCH_SUPPORT
+if [[ "$ARCH_SUPPORT" == "linux/arm64" ]]; then
+  TARGETARCH="arm64"
+elif [[ "$ARCH_SUPPORT" == "linux/amd64" ]]; then
+  TARGETARCH="amd64"
+else
+  echo "Unsupported ARCH_SUPPORT value: $ARCH_SUPPORT"
+  exit 1
+fi
+
+# Build the Docker image with the build argument
+docker buildx build --platform "$ARCH_SUPPORT" -t "$SUGARFUNGE_NODE_IMAGE":"$SUGARFUNGE_NODE_DOCKER_TAG" --build-arg TARGETARCH="$TARGETARCH" --push .
