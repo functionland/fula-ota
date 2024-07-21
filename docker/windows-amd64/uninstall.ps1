d59c16b52072a5e10b30eb3bc23c375d03d8cc7c
ehsan shariati <ehsan6sha@gmail.com>
2024-07-20 23:18:34 -0600

    corrected docker installation

--- docker/windows-amd64/uninstall.ps1
+++ docker/windows-amd64/uninstall.ps1
@@ -12,8 +12,4 @@ Write-Host "Stopping all services..."
 Write-Host "Stopping Docker containers..."
 docker-compose -f "$InstallationPath\docker-compose.yml" down -v --rmi all
 
-# Remove Docker
-Write-Host "Uninstalling Docker..."
-Start-Process "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"Uninstall-Package -Name docker -Force`"" -Verb RunAs
-
 Write-Host "Uninstallation complete."
