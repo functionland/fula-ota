905c80c4d579a56939721f74ccfe49c2dc476168
ehsan shariati <ehsan6sha@gmail.com>
2024-10-19 14:54:11 -0400

    Update fula.sh

--- docker/fxsupport/linux/fula.sh
+++ docker/fxsupport/linux/fula.sh
@@ -295,25 +295,25 @@ function install() {
     # Check if RPi.GPIO is installed
     python -c "import RPi.GPIO" 2>/dev/null || {
       echo "RPi.GPIO not found, installing..." 2>&1 | sudo tee -a $FULA_LOG_PATH
-      sudo apt-get install python3-rpi.gpio 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt-get install python3-rpi.gpio" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
+      sudo apt-get install -y python3-rpi.gpio 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt-get install python3-rpi.gpio" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
     }
 
     # Check if pexpect is installed
     python -c "import pexpect" 2>/dev/null || {
       echo "pexpect not found, installing..." 2>&1 | sudo tee -a $FULA_LOG_PATH
-      sudo apt-get install python3-pexpect 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-pexpect" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
+      sudo apt-get install -y python3-pexpect 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-pexpect" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
     }
 
     # Check if requests is installed
     python -c "import requests" 2>/dev/null || {
       echo "requests not found, installing..." 2>&1 | sudo tee -a $FULA_LOG_PATH
-      sudo apt-get install python3-requests 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-requests" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
+      sudo apt-get install -y python3-requests 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-requests" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
     }
 
     # Check if psutil is installed
     python -c "import psutil" 2>/dev/null || {
       echo "psutil not found, installing..." 2>&1 | sudo tee -a $FULA_LOG_PATH
-      sudo apt-get install python3-psutil 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-psutil" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
+      sudo apt-get install -y python3-psutil 2>&1 | sudo tee -a $FULA_LOG_PATH || { echo "Could not apt install python3-psutil" 2>&1 | sudo tee -a $FULA_LOG_PATH; all_success=false; } || true
     }
   else
     echo "Internet check failed, checking for existing dependencies..." 2>&1 | sudo tee -a $FULA_LOG_PATH
